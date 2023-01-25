54e4789706fee5cdf0cb71a5559308a202d953de
Ahmad_Mrf <44136738+AhmadMrf@users.noreply.github.com>
2023-01-25 20:55:21 +0330

    add toggle icon function

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,17 @@
-now = new Date()
-a = {d:now.toUTCString()}
+// ----------  html tags ----------
+
+const toggleMenuIcon = document.querySelector(".menu");
+const toggleThemeIcon = document.querySelector(".sidebar-toggle-theme");
+
+const aside = document.querySelector(".sidebar-content");
+
+// ----------  functions ----------
+
+const toggleIcon = (e) => {
+  e.currentTarget.classList.toggle("active");
+};
+
+// ----------  listeners  ----------
+
+toggleMenuIcon.addEventListener("click", toggleIcon);
+toggleThemeIcon.addEventListener("click", toggleIcon);
