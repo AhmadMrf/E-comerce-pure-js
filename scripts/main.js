8a307d9de63dfbabedfaba7e77eee894b3619e5e
Ahmad_Mrf <44136738+AhmadMrf@users.noreply.github.com>
2023-01-26 17:40:26 +0330

    dark mood functionality added

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
 // ----------  html tags ----------
-
-const toggleMenuIcon = document.querySelector(".menu");
-const toggleThemeIcon = document.querySelector(".sidebar-toggle-theme");
+const body = document.body;
 
 const aside = document.querySelector(".sidebar-content");
 
+const toggleMenuIcon = document.querySelector(".menu");
+const toggleThemeIcon = document.querySelectorAll(".toggle-theme");
+console.log(toggleThemeIcon);
 // ----------  functions ----------
 
 const toggleIcon = (e) => {
@@ -14,10 +15,30 @@ const toggleIcon = (e) => {
 const toggleSideBar = (e) => {
   aside.classList.toggle("active");
 };
+
+const toggleTheme = (e) => {
+  let theme;
+  body.dataset.theme === "light" ? (theme = "dark") : (theme = "light");
+  body.dataset.theme = theme;
+  localStorage.setItem("theme", theme);
+};
+
+const initialTheme = () => {
+  let theme = localStorage.getItem("theme");
+  if (theme) {
+    body.dataset.theme = theme;
+  }
+};
+initialTheme();
+
 // ----------  listeners  ----------
 
 toggleMenuIcon.addEventListener("click", (e) => {
   toggleIcon(e);
   toggleSideBar(e);
 });
-toggleThemeIcon.addEventListener("click", toggleIcon);
+toggleThemeIcon.forEach((item) =>
+  item.addEventListener("click", (e) => {
+    toggleTheme(e);
+  })
+);
