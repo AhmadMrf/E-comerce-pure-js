4f695fccfcee464fc1e7b64f484c57365984063e
Ahmad_Mrf <44136738+AhmadMrf@users.noreply.github.com>
2023-02-17 17:22:12 +0330

    add functions to index js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,101 @@
 import Swiper from "../assets/swiper/swiper.js";
+import { changeData } from "./utils/changeData.js";
+import { getTrends } from "./utils/getTrends.js";
+import { getCategories } from "./utils/getCategories.js";
+import { stars } from "./utils/stars.js";
+import { createPreLoader } from "./utils/createPreLoader.js";
+import { setLocalStorage, getLocalStorage } from "./utils/useLocalStorage.js";
+import {
+  PRODUCT_PRE_LOADER,
+  BASE_URL,
+  FILTER_BTN_PRE_LOADER,
+} from "../assets/data/template.js";
+
+const productsWrapper = document.querySelector(
+  ".new-products-container .swiper-wrapper"
+);
+const trendsWrapper = document.querySelector(
+  ".trends-container .swiper-wrapper"
+);
+const filterBtnwrapper = document.querySelector(".filters-wrapper");
+
+function mapFilterBtns(item) {
+  let active = "";
+  if (item === "all") {
+    active = "active";
+  }
+  return `
+<button class='${active}' type="button">${item}</button>
+  `;
+}
+function mapProduct(item) {
+  const totalPrice = +item.price + (+item.price * item.discount) / 100;
+  return `
+      <article class="product swiper-slide">
+              <div class="content">
+                <img src="${item.image[0]}" alt="${item.name}" />
+                <div class="tags">
+                  <span class="tag discount">${item.discount}%</span>
+                  <!-- <span class="tag new">new!</span> -->
+                </div>
+                <div class="buttons">
+                  <button class="buttons-like" type="button">
+                    <svg class="svg">
+                      <use href="./assets/icons/svg-icons.svg#icon-Heart"></use>
+                    </svg>
+                  </button>
+                  <button class="buttons-buy" type="button">
+                    <svg class="svg">
+                      <use href="./assets/icons/svg-icons.svg#icon-Bag"></use>
+                    </svg>
+                  </button>
+                  <button class="buttons-images" type="button">
+                    <svg class="svg">
+                      <use
+                        href="./assets/icons/svg-icons.svg#icon-Gallery"
+                      ></use>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              <div class="info">
+                <div class="details">
+                  <span class="category">${item.category}</span>
+                  <div class="stars">
+                    ${stars(+item.rate)}
+                  </div>
+                </div>
+                <span class="title">${item.name}</span>
+                <div class="price">
+                  <span class="current-price">$${+item.price}</span>
+                  <span class="base-price">$${totalPrice}</span>
+                </div>
+              </div>
+            </article>
+      `;
+}
+createPreLoader(trendsWrapper, PRODUCT_PRE_LOADER, 3);
+createPreLoader(productsWrapper, PRODUCT_PRE_LOADER, 3);
+createPreLoader(filterBtnwrapper, FILTER_BTN_PRE_LOADER, 4);
+
+fetch(`${BASE_URL}/products`)
+  .then((res) => res.json())
+  .then((data) => {
+    const productsContent = changeData(data);
+    const trends = getTrends(productsContent);
+    const filterBtns = getCategories(productsContent);
+
+    const mappedProduct = productsContent.map(mapProduct);
+    const mappedTrends = trends.map(mapProduct);
+    const mappedFilterBtns = filterBtns.map(mapFilterBtns);
+
+    productsWrapper.innerHTML = mappedProduct.join("");
+    trendsWrapper.innerHTML = mappedTrends.join("");
+    filterBtnwrapper.innerHTML = mappedFilterBtns.join("");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // ---------- swiper  -------------
 const swiperBreakPoints = {
@@ -42,6 +139,7 @@ const trends_swiper = new Swiper(".trends-container .products-wrapper", {
 const reviews_swiper = new Swiper(".review-wrapper", {
   loop: true,
   slidesPerView: 1,
+  spaceBetween: 25,
   navigation: {
     nextEl: ".review-wrapper .key.right",
     prevEl: ".review-wrapper .key.left",
