adc4d78f51d6b8e6549d15be36fe0c43f138d2d1
Ahmad_Mrf <44136738+AhmadMrf@users.noreply.github.com>
2023-02-17 17:21:45 +0330

    add temp data file

---
+++ assets/data/template.js
@@ -0,0 +1,37 @@
+export const PRODUCT_PRE_LOADER = `
+<article class="pre-loader product swiper-slide">
+  <div class="content">
+  <div class="buttons">
+  <button class="buttons-like" type="button"></button>
+  <button class="buttons-buy" type="button"></button>
+  <button class="buttons-images" type="button"></button>
+  </div>
+  </div>
+</article>`;
+export const REVIEW_PRE_LOADER = `
+<div class="pre-loader swiper-slide review-info">
+  <div class="image-wrapper"></div>
+  <div class="review-info-content"></div>
+</div>`;
+export const FILTER_BTN_PRE_LOADER = `
+<button class='pre-loader' type="button"></button>
+`;
+export const FULL_STAR = `
+<svg class="svg star-full">
+<use href="./assets/icons/svg-icons.svg#icon-Star"></use>
+</svg>
+`;
+export const HALF_STAR = `
+<svg class="svg star-half">
+<use href="./assets/icons/svg-icons.svg#icon-Star"></use>
+</svg>
+`;
+export const EMPTY_STAR = `
+<svg class="svg star-empty">
+<use href="./assets/icons/svg-icons.svg#icon-Star"></use>
+</svg>
+`;
+
+export const BASE_URL = `https://635298c0a9f3f34c37434ca1.mockapi.io`;
+
+export const CATEGORIES = ["kids", "men", "women", "winter"];
