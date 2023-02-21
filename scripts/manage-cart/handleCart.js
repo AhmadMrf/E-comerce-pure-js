8bb738965284b206d82ace90b34d9c3dace0ccce
mrAhmad <naashna20@gmail.com>
2023-02-21 16:57:24 +0330

    modify handle cart and add remove item fn

--- scripts/manage-cart/handleCart.js
+++ scripts/manage-cart/handleCart.js
@@ -2,11 +2,15 @@ import { setLocalStorage, getLocalStorage } from "../utils/useLocalStorage.js";
 export function resetCart() {
   setLocalStorage("products", []);
 }
+
+export function removeitem(id) {
+  let cartData = getLocalStorage("products");
+  cartData = cartData.filter((cartItem) => +cartItem.id !== +id);
+  setLocalStorage("products", cartData);
+  return cartData;
+}
 export function handleCart(allProduct, id, toggle) {
   let cartData = getLocalStorage("products");
-  const removeitem = (id) => {
-    cartData = cartData.filter((cartItem) => +cartItem.id !== +id);
-  };
   const product = allProduct.find((productItem) => +productItem.id === +id);
 
   if (product) {
@@ -18,10 +22,10 @@ export function handleCart(allProduct, id, toggle) {
           toggle === "plus"
             ? (cartItem.quantity += 1)
             : (cartItem.quantity -= 1);
-          if (cartItem.quantity <= 0) removeitem(cartItem.id);
         }
         return cartItem;
       });
+      if (isInCart.quantity <= 0) cartData = removeitem(isInCart.id);
     } else {
       const newCartProduct = { ...product, quantity: 1 };
       cartData = [...cartData, newCartProduct];
