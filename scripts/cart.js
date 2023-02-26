e95c299535e647ec4258562578b5ad6b6a613738
Ahmad_Mrf <44136738+AhmadMrf@users.noreply.github.com>
2023-02-27 03:06:05 +0330

    complete cart functionality

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,17 +1,51 @@
 import { handleBadge } from "./manage-cart/handleBadge.js";
+import { handleCart } from "./manage-cart/handleCart.js";
 import { getLocalStorage } from "./utils/useLocalStorage.js";
 import { insertData } from "./utils/insertData.js";
 import { addEventListenerFn } from "./utils/addEventListenerFn.js";
 
-// ---------- selectors
+// ---------- selectors -----------
 const cartWrapper = document.querySelector(".cart-items-wrapper");
 const countCartItem = document.querySelector(".item-count");
 const totalPrice = document.querySelector(".total-price-amount");
 const totalDiscount = document.querySelector(".total-discount-amount");
 
 // ---------- insert local data to page --------------
-const cartData = getLocalStorage("products");
-function toggleCartItem() {}
+let cartData = getLocalStorage("products");
+
+function resetCart() {
+  cartData = handleCart(null, "reset");
+  cartWrapper.innerHTML = "";
+  calcCountCartItem(countCartItem);
+  calcTotalItem(totalPrice, "price");
+  calcTotalItem(totalDiscount, "discount");
+}
+function toggleCartItem(productBtnsParent_withId, toggle) {
+  const id = productBtnsParent_withId.dataset.product_id;
+  const product = cartData.find((productItem) => +productItem.id === +id);
+
+  cartData = handleCart(product, toggle);
+  updateCartUi(productBtnsParent_withId, id);
+}
+function updateCartUi(parent, id) {
+  const product = cartData.find((productItem) => +productItem.id === +id);
+  if (!product) {
+    parent.remove();
+    calcCountCartItem(countCartItem);
+    calcTotalItem(totalPrice, "price");
+    calcTotalItem(totalDiscount, "discount");
+    return;
+  }
+
+  const itemCount = parent.querySelector(".change-count span");
+  const subTotal = parent.querySelector(".subtotal");
+  itemCount.textContent = product.quantity;
+  subTotal.textContent = +product.price * product.quantity;
+
+  calcTotalItem(totalPrice, "price");
+  calcTotalItem(totalDiscount, "discount");
+  calcCountCartItem(countCartItem);
+}
 function mapCartItem(item) {
   const totalPrice = +item.price + (+item.price * item.discount) / 100;
 
@@ -22,7 +56,9 @@ function mapCartItem(item) {
       <img src="${item.image[0]}" alt="${item.name}" />
     </div>
     <div class="item-info-container">
-      <h4 class="name">${item.name}</h4>
+      <h4 class="name"><a href='./product.html#${item.id}' >${
+    item.name
+  }</a></h4>
       <span class="category">${item.category}</span>
       <div class="price">
         <span class="current-price">$${+item.price}</span>
@@ -64,30 +100,42 @@ function mapCartItem(item) {
 }
 insertData(cartWrapper, cartData, mapCartItem);
 
-const plusButtons = cartWrapper.querySelectorAll(".change-count .plus");
-const minusButtons = cartWrapper.querySelectorAll(".change-count .minus");
+const plusButtons = cartWrapper.querySelectorAll(".item-buy-info .plus");
+const minusButtons = cartWrapper.querySelectorAll(".item-buy-info .minus");
+const removeItemButtons = cartWrapper.querySelectorAll(".item-buy-info .trash");
+const resetCartButton = document.querySelector(".delete-cart-wrapper .trash");
 
 addEventListenerFn(plusButtons, (e) => {
-  console.log("add");
+  const parent = e.currentTarget.closest("[data-product_id]");
+  toggleCartItem(parent, "plus");
 });
 addEventListenerFn(minusButtons, (e) => {
-  console.log("remove");
+  const parent = e.currentTarget.closest("[data-product_id]");
+  toggleCartItem(parent, "minus");
+});
+addEventListenerFn(removeItemButtons, (e) => {
+  const parent = e.currentTarget.closest("[data-product_id]");
+  toggleCartItem(parent, "remove");
+});
+addEventListenerFn(resetCartButton, () => {
+  resetCart();
 });
 // ------------ calculate item counts ---------------
-function calcCountCartItem() {
-  const count = cartData.length;
+function calcCountCartItem(element) {
+  let count = cartData.length;
   if (count) {
-    return count === 1 ? `${count} item` : `${count} items`;
+    count = count === 1 ? `${count} item` : `${count} items`;
   } else {
-    return `no items`;
+    count = `no items`;
   }
+  element.textContent = count;
 }
-function calcTotalItem(item) {
-  const cartData = getLocalStorage("products");
+function calcTotalItem(element, item) {
   const totals = cartData.reduce(
     (total, cartItem) => {
       const price = +cartItem.price * cartItem.quantity;
-      const discount = (+cartItem.price * cartItem.discount) / 100;
+      const discount =
+        ((+cartItem.price * cartItem.discount) / 100) * cartItem.quantity;
       return {
         price: total.price + price,
         discount: total.discount + discount,
@@ -98,12 +146,13 @@ function calcTotalItem(item) {
       discount: 0,
     }
   );
-  return totals;
+  const discount = totals.discount.toFixed(2);
+  const price = totals.price.toFixed(2);
+  element.textContent = item === "price" ? price : discount;
 }
 
-countCartItem.textContent = calcCountCartItem();
-
-totalPrice.textContent = `$${calcTotalItem().price}`;
-totalDiscount.textContent = `$${calcTotalItem().discount}`;
+calcCountCartItem(countCartItem);
+calcTotalItem(totalPrice, "price");
+calcTotalItem(totalDiscount, "discount");
 // ---------- cart and favorite badge  -------------
 handleBadge("favorite");
