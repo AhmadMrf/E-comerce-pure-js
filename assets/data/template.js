a7c33a5176436cad27f10c50dfc819455f712b21
Ahmad_Mrf <44136738+AhmadMrf@users.noreply.github.com>
2023-03-03 20:57:14 +0330

    add colors

--- assets/data/template.js
+++ assets/data/template.js
@@ -66,3 +66,4 @@ export const EMPTY_STAR = `
 export const BASE_URL = `https://635298c0a9f3f34c37434ca1.mockapi.io`;
 
 export const CATEGORIES = ["kids", "men", "women", "winter"];
+export const COLORS = ["#ff0000", "#00ff00", "#0000ff"];
