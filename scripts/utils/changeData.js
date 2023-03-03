923ddf11fee7f980f743882f85b93e990e513153
Ahmad_Mrf <44136738+AhmadMrf@users.noreply.github.com>
2023-03-03 20:58:49 +0330

    add colors and modify categories

--- scripts/utils/changeData.js
+++ scripts/utils/changeData.js
@@ -1,8 +1,13 @@
-import { CATEGORIES } from "../../assets/data/template.js";
+import { CATEGORIES, COLORS } from "../../assets/data/template.js";
 export function changeData(data) {
   const newData = data.map((item) => {
     const randomImageId = Math.floor(Math.random() * 400);
-    const category = CATEGORIES[Math.floor(Math.random() * CATEGORIES.length)];
+    const category = [
+      CATEGORIES[Math.floor(Math.random() * CATEGORIES.length)],
+    ];
+    const colorsCount = Math.floor(Math.random() * COLORS.length);
+    const colors = COLORS.slice(0, colorsCount + 1);
+    const count_in_stock = +item.count_in_stock <= 3 ? 0 : +item.count_in_stock;
     const images = [
       `https://picsum.photos/id/${randomImageId}/200/300`,
       `https://picsum.photos/id/${randomImageId + 1}/200/300`,
@@ -15,6 +20,8 @@ export function changeData(data) {
       discount: +item.discount * 10,
       rate: +item.rate / 2,
       image: images,
+      colors,
+      count_in_stock,
     };
   });
   return newData;
