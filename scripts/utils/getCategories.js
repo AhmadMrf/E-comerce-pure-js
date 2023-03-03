dd43d303c7dd8c3377745f7f33b1fc44b2bbe400
Ahmad_Mrf <44136738+AhmadMrf@users.noreply.github.com>
2023-03-03 20:57:42 +0330

    modify categories

--- scripts/utils/getCategories.js
+++ scripts/utils/getCategories.js
@@ -1,5 +1,5 @@
 export function getCategories(data = []) {
-  const allCategories = data.map((item) => {
+  const allCategories = data.flatMap((item) => {
     return item.category;
   });
   const categories = new Set(allCategories);
