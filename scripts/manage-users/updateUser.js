397f676d3381bd018d71ab57167ac934017d0877
Ahmad_Mrf <44136738+AhmadMrf@users.noreply.github.com>
2023-04-12 11:47:39 +0330

    modify - update user fn

--- scripts/manage-users/updateUser.js
+++ scripts/manage-users/updateUser.js
@@ -1,6 +1,11 @@
 import { setLocalStorage, getLocalStorage } from "../utils/useLocalStorage.js";
 export function updateUser(userObject) {
   const users = getLocalStorage("users");
-  const newUsers = [...users, userObject];
-  setLocalStorage("users", newUsers);
+  const updatedUsers = users.map((user) => {
+    if (user.email === userObject.email) {
+      user = { ...userObject };
+    }
+    return user;
+  });
+  setLocalStorage("users", updatedUsers);
 }
